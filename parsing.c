1cb00b297a2d30810c328efd7237ee9c609dbbdf
Zakaria El Ghabri <bigzakaria667@MacBook-Pro-du-Big-Zakaria.local>
2025-03-03 22:31:14 +0100

    list & parsing OK

--- parsing.c
+++ parsing.c
@@ -6,11 +6,23 @@
 /*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:17:33 by zel-ghab          #+#    #+#             */
-/*   Updated: 2025/03/03 00:56:17 by zel-ghab         ###   ########.fr       */
+/*   Updated: 2025/03/03 22:27:38 by zel-ghab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_put_into_list(char **argv, t_list **stack_a)
+{
+	int	i;
+
+	i = 0;	
+	if (ft_check_double(argv) != 0)
+		return (1);
+	while (argv[i])
+		list_insert(stack_a, ft_atoi(argv[i++]));
+	return (0);
+}
 
 int	ft_check_double(char **argv)
 {
@@ -35,15 +47,19 @@ int	ft_check_double(char **argv)
 	return (0);
 }
 
-int	ft_check_arguments(char **argv)
+int	ft_check_arguments(char **argv, t_list **stack_a)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	j = 0;
+	j = 1;
 	while (argv[i])
 	{
+		if (!ft_isdigit(argv[i][0]) && argv[i][0] != '+' && argv[i][0] != '-')
+			return (1);
+		if ((argv[i][0] == '-' || argv[i][0] == '+') && !ft_isdigit(argv[i][1]))
+			return (1);
 		while (argv[i][j])
 		{
 			if (!ft_isdigit(argv[i][j]))
@@ -51,9 +67,9 @@ int	ft_check_arguments(char **argv)
 			j++;
 		}
 		i++;
-		j = 0;
+		j = 1;
 	}
-	if (ft_check_double(argv) != 0)
+	if (ft_put_into_list(argv, stack_a) == 1)
 		return (1);
 	return (0);
 }
@@ -66,18 +82,19 @@ int	ft_check_argument(char *argv, t_list **stack_a)
 	if (!argv)
 		return (1);
 	i = 0;
-	while ((ft_isdigit(argv[i]) || argv[i] == ' ') && argv[i])
-		i++;
-	if (!(ft_isdigit(argv[i]) || argv[i] == ' ') && argv[i] != '\0')
+	while (argv[i])
+	{
+		if ((argv[i] == '-' || argv[i] == '+') && !ft_isdigit(argv[i + 1]))
 			return (1);
-	i = 0;
+		if (!ft_isdigit(argv[i]) && argv[i] != ' ' && argv[i] != '-' && argv[i] != '+')
+			return (1);
+		i++;	
+	}
 	spliter = ft_split(argv, ' ');
 	if (!spliter)
 		return (1);
-	if (ft_check_double(spliter) != 0)
+	if(ft_put_into_list(spliter, stack_a) == 1)
 		return (1);
-	while (spliter[i])
-		list_insert(stack_a, ft_atoi(spliter[i++]));
 	return (0);
 }
 
@@ -88,6 +105,6 @@ int	ft_checker(int argc, char **argv, t_list **stack_a)
 	else if (argc == 2)
 		return (ft_check_argument(argv[1], stack_a));
 	else
-		return (ft_check_arguments(argv + 1));
-	return (0);	
+		return (ft_check_arguments(argv + 1, stack_a));
+	return (0);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:17:33 by zel-ghab          #+#    #+#             */
-/*   Updated: 2025/03/03 00:48:09 by zel-ghab         ###   ########.fr       */
+/*   Updated: 2025/03/03 21:30:17 by zel-ghab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 	if (ft_checker(argc, argv, &stack_a) != 0)
 		return (ft_printf("%s", "Error!"));
 	else
-		return (ft_printf("%s", "OK!\n"));
+		ft_printf("%s", "OK!\n");
 	list_print(stack_a);
 	return (0);
 }
