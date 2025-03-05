7cebad632a8a6a8d7939f9aeb8fda7f9e6dfdfdb
Zakaria El Ghabri <bigzakaria667@MacBook-Pro-du-Big-Zakaria.local>
2025-03-05 20:03:31 +0100

    parsing, list & movement OK

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: zel-ghab <zel-ghab@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:17:33 by zel-ghab          #+#    #+#             */
-/*   Updated: 2025/03/05 17:46:28 by zel-ghab         ###   ########.fr       */
+/*   Updated: 2025/03/05 20:03:25 by zel-ghab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,9 @@ int	main(int argc, char **argv)
 
 	stack_a = NULL;
 	stack_b = NULL;
-	list_insert(&stack_b, 12);
-	list_insert(&stack_b, 2);
 	if (ft_checker(argc, argv, &stack_a) != 0)
 		return (ft_printf("%s", "Error!"));
 	else
 		ft_printf("%s", "OK!\n");
-	list_print(stack_a);
-	ft_rrotate(&stack_a);
-	ft_printf("%s", "AFTER :\n");
-	list_print(stack_a);
 	return (0);
 }
